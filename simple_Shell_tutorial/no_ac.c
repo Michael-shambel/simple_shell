8861f3f97481c6ba4567f5fbd737d6eda3695dab
Michael Shambel <Michaelshambel10@gmail.com>
2023-11-09 14:57:04 -0800

    this commit is for study purpose

---
+++ simple_Shell_tutorial/no_ac.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	int i;
+	int sum = 0;
+
+
+
+	
+	printf("argv or av is -\n" );
+	while (*av != NULL)
+	{
+		printf("argv[] = %s\n",*av);
+		av++;
+	}
+	
+
+	
+
+
+	return (0);
+}
